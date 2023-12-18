c689533c487e8246e20d4c5e5a095f60795715c6
Avneesh Sarwate <avneeshsarwate@gmail.com>
2023-12-18 10:04:46 -0500

    partA day 4 done

--- python/day5/challengeA.py
+++ python/day5/challengeA.py
@@ -1,40 +1,67 @@
-inputFile = "../../input/day5/inputA.txt"
+inputFile = "../../inputs/day5/input.txt"
 
 
 
-def getFromMap(map, key):
-  if key in map:
-    return map[key]
-  else:
-    return key
+def getFromMap(mapRangeLines, key):
+  outId = -1
+  for rl in mapRangeLines:
+    outId = checkIfKeyInRangeLine(key, rl)
+    if outId > -1:
+      return outId
+  
+  return key
 
-def fillMapFromLine(dest, start, rangeLen, map):
-  for i in range(rangeLen):
-    map[start+i] = dest+i
+def checkIfKeyInRangeLine(key, rangeLine): #returns -1 if not in range
+  end = rangeLine[0]
+  start = rangeLine[1]
+  width = rangeLine[2]
+  if start <= key < start + width:
+    # print("        in range", end, start, width, "in", key, "out", end + (key-start))
+    return end + (key-start)
+  else: 
+    return -1
 
 def parseMapDef(mapDefString): #returns: (srcStr,destStr), map
   mapDefLines = mapDefString.split("\n")
   mapTypeSplit = mapDefLines[0].split(" ")[0].split("-")
-  rangeLines = [int(rl.split(" ")) for rl in mapDefLines[1:]]
-  stageMap = {}
-  for rl in rangeLines:
-    fillMapFromLine(rl[0], rl[1], rl[2], stageMap)
+  rangeLines = [[int(n) for n in rl.split(" ")] for rl in mapDefLines[1:]]
+  stageMap = rangeLines
   
   return (mapTypeSplit[0], mapTypeSplit[-1]), stageMap
 
-def parseMaps(inputString):
-  mapDefStrings = inputString.split("\n\n")
-  mapDefs = [parseMapDef(mds) for mds in mapDefStrings]
-  return mapDefs
+def parseInput(inputString):
+  inputBlocks = inputString.split("\n\n")
+  mapDefs = [parseMapDef(mds) for mds in inputBlocks[1:]]
+
+  startSeeds = [int(s) for s in inputBlocks[0].split(": ")[1].split(" ")]
+
+  return mapDefs, startSeeds
 
 
-def convertSeed(seed, mapChain):
+def convertSeed(seed, mapDefs):
   translatedId = seed
   
-  for toFrom, stageMap in mapChain:
+  for toFrom, stageMap in mapDefs:
     translatedId = getFromMap(stageMap, translatedId)
+    # print("    ", toFrom[1], translatedId, "\n")
   
   return translatedId
 
+def findLowestSeedMap(inputString):
+  mapDefs, startSeeds = parseInput(inputString)
+  lowestLoc = float("inf")
+  for seed in startSeeds:
+    # print("convertingSeed ", seed)
+    loc = convertSeed(seed, mapDefs)
+    if loc < lowestLoc:
+      lowestLoc = loc
+  
+  return lowestLoc
+
+
 if __name__ == "__main__":
-  x = 5
+
+  inputStr = open(inputFile).read()
+  lowest = findLowestSeedMap(inputStr)
+
+  print("lowest: ", lowest)
